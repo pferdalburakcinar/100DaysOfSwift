82601449df8aa90ada5b8ca2b727954f83ff1706
ecinar <erdalburakcinar@gmail.com>
2021-06-30 15:07:24 +0300

    project 7 until wrap completed

---
+++ Project7/Project7/Petition.swift
@@ -0,0 +1,14 @@
+//
+//  Petition.swift
+//  Project7
+//
+//  Created by ecinar on 30.06.2021.
+//
+
+import Foundation
+
+struct Petition: Codable {
+    var title: String
+    var body: String
+    var signatureCount: Int
+}
