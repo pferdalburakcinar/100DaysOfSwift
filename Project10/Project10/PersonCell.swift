4721bdffd6665a81884d336afaf4b9d916cbc04f
ecinar <erdalburakcinar@gmail.com>
2021-07-05 18:46:19 +0300

    day 42 completed

---
+++ Project10/Project10/PersonCell.swift
@@ -0,0 +1,14 @@
+//
+//  Person.swift
+//  Project10
+//
+//  Created by ecinar on 5.07.2021.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var name: UILabel!
+}
---
+++ Project10/Project10/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  Project10
+//
+//  Created by ecinar on 5.07.2021.
+//
+
+import UIKit
+
+class ViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Person", for: indexPath) as? PersonCell else {
+            fatalError("unable to dequee PersonCell ")
+        }
+        
+        return cell
+    }
+
+}
+
