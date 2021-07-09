e69b6aa3a42b8b0330911edf99d16711dfa2cc42
ecinar <erdalburakcinar@gmail.com>
2021-07-09 17:44:05 +0300

    day 48 completed

---
+++ Project10b/Project10/Person.swift
@@ -0,0 +1,20 @@
+//
+//  Person.swift
+//  Project10
+//
+//  Created by ecinar on 9.07.2021.
+//
+
+import UIKit
+
+class Person: NSObject {
+
+    var name: String
+    var image: String
+    
+     init(name: String, image: String) {
+        self.name = name
+        self.image = image
+    }
+    
+}
