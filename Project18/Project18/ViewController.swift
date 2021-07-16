57df5b2bce35ab639774f5a78eda2d03fa08bfe0
ecinar <erdalburakcinar@gmail.com>
2021-07-16 21:01:17 +0300

    day 67 completed

---
+++ Project18/Project18/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project18
+//
+//  Created by ecinar on 14.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Project19/Extension/Action
@@ -0,0 +1,16 @@
+
+var Action = function() {};
+
+Action.prototype = {
+
+run: function(parameters) {
+
+},
+
+finalize: function(parameters) {
+
+}
+
+};
+
+var ExtensionPreprocessingJS = new Action
